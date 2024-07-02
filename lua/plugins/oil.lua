e5a92a39f61a3a9af143ea8c1d3b6e830f4f6fee
Hady Osman <hadyos@gmail.com>
2024-07-02 14:18:30 +1200

    Add Oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    cmd = { "Oil" },
+    opts = {
+      float = {
+        max_width = 100,
+        max_height = 50,
+      },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "<Leader>o", ":Oil --float<CR>", mode = "", desc = "Open Oil" },
+    },
+  },
+}
