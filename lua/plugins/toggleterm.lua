bff5b90868d3ab146bba0a496883d65759e30a37
Hady Osman <hadyos@gmail.com>
2024-07-02 16:34:00 +1200

    add toggle term shortcuts

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,3 +1,5 @@
+local toggleTerm = require("toggleterm")
+
 return {
   {
     "akinsho/toggleterm.nvim",
@@ -6,6 +8,34 @@ return {
     opts = {
       direction = "float",
       open_mapping = [[<C-`>]],
+    },
+    keys = {
+      {
+        "<leader>tv",
+        function()
+          local count = vim.v.count1
+          toggleTerm.toggle(count, 0, vim.loop.cwd(), "vertical")
+        end,
+        desc = "ToggleTerm (vertical)",
+      },
+      {
+        "<leader>th",
+        function()
+          local count = vim.v.count1
+          toggleTerm.toggle(count, 10, vim.loop.cwd(), "horizontal")
+        end,
+        desc = "ToggleTerm (horizontal)",
+      },
+    },
+  },
+
+  -- Register the `<leader>h` prefix as with a group name in which-key.
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>t"] = { name = "+ToggleTerm" },
+      },
     },
   },
 }
