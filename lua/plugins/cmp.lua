796b26c04b6c71fd1e1c3246918169c7a3786b90
Hady Osman <hadyos@gmail.com>
2024-07-15 23:11:45 +1200

    Fix copilot plugin config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,54 +1,33 @@
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+  {
+    "hrsh7th/nvim-cmp",
 
-    local cmp = require("cmp")
+    -- Override LazyVim's default configuration
+    config = function(_, opts)
+      local cmp = require("cmp")
 
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<CR>"] = cmp.mapping({
-        i = function(fallback)
-          if cmp.visible() and cmp.get_active_entry() then
-            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-          else
-            fallback()
-          end
-        end,
-        s = cmp.mapping.confirm({ select = true }),
-        c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-      }),
+      -- Disable autcomplete as it can be too noisey
+      opts.completion = {
+        autocomplete = false,
+      }
 
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          print("visible")
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
-        elseif vim.snippet.active({ direction = 1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(1)
-          end)
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
+      -- Add borders for more clarity
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
 
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif vim.snippet.active({ direction = -1 }) then
-          vim.schedule(function()
-            vim.snippet.jump(-1)
-          end)
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    })
-  end,
+      -- Mimic VSCode; both tab and enter will confirm the selection
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "s" }),
+        ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "s" }),
+      });
+
+      cmp.setup(opts)
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    enabled = false
+  }
 }
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,16 +1,44 @@
 return {
   {
     "zbirenbaum/copilot.lua",
+    dependencies = {
+      { "hrsh7th/nvim-cmp" },
+    },
+
+    cmd = "Copilot",
+    event = "InsertEnter",
+    build = ":Copilot auth",
+
     opts = {
-      suggestion = {
-        enabled = true
-      },
       panel = {
-        enabled = true
-      }
-    }
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        -- use the built-in keymapping for "accept" (<M-l>)
+        auto_trigger = true,
+        accept = false, -- disable built-in keymapping
+        keymap = {
+          accept = "<Tab>",
+        },
+      },
+      filetypes = {},
+    },
+    config = function(_, opts)
+      require("copilot").setup(opts)
+
+      -- hide copilot suggestions when cmp menu is open
+      -- to prevent odd behavior/garbled up suggestions
+      local cmp_status_ok, cmp = pcall(require, "cmp")
+      if cmp_status_ok then
+        cmp.event:on("menu_opened", function()
+          vim.b.copilot_suggestion_hidden = true
+        end)
+
+        cmp.event:on("menu_closed", function()
+          vim.b.copilot_suggestion_hidden = false
+        end)
+      end
+    end,
   },
-  {
-    "zbirenbaum/copilot-cmp",
-  }
 }
